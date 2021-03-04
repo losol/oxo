ee7716f40ea97502073d280625f00ddefdd90353
Ole Kristian Losvik <ole@losol.no>
2021-03-04 18:58:32 +0100

    Init

---
+++ src/oxo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace oxo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        var versionString = Assembly.GetEntryAssembly()
+                                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                .InformationalVersion
+                                .ToString();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("**");
+        Console.WriteLine($"oxo v{versionString}");
+        Console.WriteLine("**");
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\nUsage:");
+        Console.WriteLine("  botsay <message>");
+        Console.WriteLine("");
+        return;
+      }
+    }
+  }
+}
